cac77ae025e76671025b4e8952165ed8536049b7
Pepibar <felipebargros@gmail.com>
2025-06-26 15:44:05 -0300

    prueba

--- neon.js
+++ neon.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import pkg from 'pg';
 
 dotenv.config();
+console.log('dotenv cargado, JWT_SECRET:', process.env.JWT_SECRET);
 const { Pool } = pkg;
 
 const pool = new Pool({
