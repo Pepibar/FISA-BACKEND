93cecc066feb4358d4eb0f8e040568ab795d7085
Pepibar <felipebargros@gmail.com>
2025-06-30 20:54:13 -0300

    arreglos funcion mail

--- emails.js
+++ emails.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export async function sendEmail({ to, subject, text, html }) {
+async function enviarMail({ to, subject, text, html }) {
   const mailOptions = {
     from: `"FISA App" <${process.env.SMTP_USER}>`,
     to,
